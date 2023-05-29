5e201af6fa1bc88f74b19c8c5093d3c1f4c2201b
JamieChen007 <107536976+JamieChen007@users.noreply.github.com>
2023-05-29 15:25:35 +1000

    first commit

---
+++ top-social client/src/js/common.js
@@ -0,0 +1,75 @@
+export const navbar_logo = document.querySelector(".navbar_logo");
+
+export const navbar_profileImage = document.querySelector(
+  ".navbar_profileImage"
+);
+
+export const navbar_create = document.querySelector(".navbar_create");
+
+export const navbar_search_input = document.querySelector(
+  ".navbar_search_input"
+);
+
+export const main_left_create = document.querySelector(".main_left_create");
+
+export const main_left_profile_profileImage = document.querySelector(
+  ".main_left_profile_profileImage"
+);
+
+export const main_left_profile_nameandat_name = document.querySelector(
+  ".main_left_profile_nameandat_name"
+);
+
+export const main_left_profile_nameandat_at = document.querySelector(
+  ".main_left_profile_nameandat_at"
+);
+
+export const main_left_menucontainer = document.querySelector(
+  ".main_left_menucontainer"
+);
+
+export const main_middle_highlinecontainer = document.querySelector(
+  ".main_middle_highlinecontainer"
+);
+
+export const main_middle_post_profileimage = document.querySelector(
+  ".main_middle_post_profileimage"
+);
+
+export const main_middle_post = document.querySelector(".main_middle_post");
+
+export const main_middle_post_postbtn = document.querySelector(
+  ".main_middle_post_postbtn"
+);
+
+export const main_middle_post_input = document.querySelector(
+  ".main_middle_post_input"
+);
+
+export const main_middle_cardcontainer = document.querySelector(
+  ".main_middle_cardcontainer"
+);
+
+export const main_right_message_title_name = document.querySelector(
+  ".main_right_message_title_name"
+);
+
+export const main_right_message_search_input = document.querySelector(
+  ".main_right_message_search_input"
+);
+
+export const main_right_message_tab_menu = document.querySelectorAll(
+  ".main_right_message_tab_menu"
+);
+
+export const main_right_message_container = document.querySelector(
+  ".main_right_message_container"
+);
+
+export const main_right_requests_title = document.querySelector(
+  ".main_right_requests_title"
+);
+
+export const main_right_requests = document.querySelector(
+  ".main_right_requests"
+);
